7771acf13d432799874af7262f23f99bee971787
biggwang <ghmryg@gmail.com>
2024-07-24 22:53:19 +0900

    ios date, review, rating 추출 되도록 수정

--- ios_scraper.py
+++ ios_scraper.py
@@ -2,7 +2,7 @@ from app_store_scraper import AppStore
 from datetime import datetime
 from pprint import pprint
 
-def get_ios_reviews(keywords):
+def get_reviews(keywords):
     kakao_pay = AppStore(country="kr", app_name="kakaopay", app_id="847268987")
     kakao_pay.review(how_many=30)
     
@@ -11,7 +11,11 @@ def get_ios_reviews(keywords):
     for item in kakao_pay.reviews:
         review_text = item['review'].lower()
         if any(keyword.lower() in review_text for keyword in keywords):
-            ##pprint(item['review'])
-            filtered_reviews.append(item['review'])
+            filtered_review = {
+                'date': item['date'].strftime('%y-%m-%d %H:%M:%S'),
+                'review': item['review'],
+                'rating': item['rating']
+            }
+            filtered_reviews.append(filtered_review)
     
     return filtered_reviews
